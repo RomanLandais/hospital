2114fcfd7924e809cb49920ca7d552399683f307
RomanLandais <roman.landais@gmail.com>
2024-05-19 16:04:30 +0400

    maj signIn

--- src/app/features/component/new-stay/new-stay.component.ts
+++ src/app/features/component/new-stay/new-stay.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ComServerService } from '../../../shared/services/com-server.service';
+import { TokenService } from '../../../shared/services/auth/token.service';
 
 @Component({
   selector: 'app-new-stay',
   standalone: true,
   imports: [],
   templateUrl: './new-stay.component.html',
-  styleUrl: './new-stay.component.css'
+  styleUrl: './new-stay.component.css',
 })
-export class NewStayComponent {
+export class NewStayComponent implements OnInit {
+  newStayForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private comServerService: ComServerService,
+    private tokenService: TokenService
+  ) {}
+
+  ngOnInit() {
+    this.newStayForm = this.fb.group({
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      reason: ['', Validators.required],
+      specialty: ['', Validators.required],
+      doctor: ['', Validators.required],
+    });
+  }
 }
