71b1d4b6b9e85e29bb24342e730d4a4071edcce6
RomanLandais <roman.landais@gmail.com>
2024-05-19 12:31:30 +0400

    Tokens ok pour vérif données sortantes frontENd

--- src/app/shared/services/com-server.service.ts
+++ src/app/shared/services/com-server.service.ts
@@ -1,11 +1,13 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ComServerService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   sendDataSignUp(data: any, endpoint: string) {
     // Concaténez l'endpoint fourni à l'URL de base
@@ -18,6 +20,16 @@ export class ComServerService {
   }
 
   sendData(data: any, endpoint: string, authToken: string) {
+    // Vérifier si le token est présent
+    if (!authToken) {
+      // Rediriger vers la page de login
+      this.router.navigate(['/login']);
+      alert('Veuillez-vous authentifier ou vous inscrire svp');
+      return throwError(
+        () => new Error('Veuillez-vous authentifier ou vous inscrire svp')
+      );
+    }
+
     const url = `https://localhost:3000/api/hospital/${endpoint}`;
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
